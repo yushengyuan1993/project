d64f3d23a4aa8ee36a7c1575e9eba75722b5482d
yushare <582718643@qq.com>
2018-12-19 19:36:59 +0800

    update

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,208 @@
 <template>
   <div class="index">
-    index page
+    <v-app light>
+      <v-toolbar class="white">
+        <v-toolbar-title v-text="title"></v-toolbar-title>
+      </v-toolbar>
+      <v-content>
+        <section>
+          <v-parallax src="images/2.jpg" height="600">
+            <v-layout
+              column
+              align-center
+              justify-center
+              class="white--text"
+            >
+              <img src="images/logo.png" alt="Vuetify.js" height="100">
+              <h1 class="white--text mb-2 display-1 text-xs-center">Write something here when I have time.</h1>
+              <div class="subheading mb-3 text-xs-center">欢迎交流</div>
+              <v-btn
+                class="blue lighten-2 mt-5"
+                dark
+                large
+                href="https://www.yvshare.cn/"
+              >
+                回到首页
+              </v-btn>
+            </v-layout>
+          </v-parallax>
+        </section>
+
+        <section>
+          <v-layout
+            column
+            wrap
+            class="my-5"
+            align-center
+          >
+            <v-flex xs12 sm4 class="my-3">
+              <div class="text-xs-center">
+                <h2 class="headline">Demo list</h2>
+                <!-- <span class="subheading"> -->
+                  <!-- Cras facilisis mi vitae nunc  -->
+                <!-- </span> -->
+              </div>
+            </v-flex>
+            <v-flex xs12>
+              <v-container grid-list-xl>
+                <v-layout row wrap align-center>
+                  <v-flex xs12 md4>
+                    <v-card class="elevation-0 transparent">
+                      <v-card-text class="text-xs-center">
+                        <v-icon x-large class="blue--text text--lighten-2">color_lens</v-icon>
+                      </v-card-text>
+                      <v-card-title primary-title class="layout justify-center">
+                        <div class="headline text-xs-center">Material Design</div>
+                      </v-card-title>
+                      <v-card-text>
+                        Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                        Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
+                      </v-card-text>
+                    </v-card>
+                  </v-flex>
+                  <v-flex xs12 md4>
+                    <v-card class="elevation-0 transparent">
+                      <v-card-text class="text-xs-center">
+                        <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
+                      </v-card-text>
+                      <v-card-title primary-title class="layout justify-center">
+                        <div class="headline">Fast development</div>
+                      </v-card-title>
+                      <v-card-text>
+                        Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                        Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
+                      </v-card-text>
+                    </v-card>
+                  </v-flex>
+                  <v-flex xs12 md4>
+                    <v-card class="elevation-0 transparent">
+                      <v-card-text class="text-xs-center">
+                        <v-icon x-large class="blue--text text--lighten-2">build</v-icon>
+                      </v-card-text>
+                      <v-card-title primary-title class="layout justify-center">
+                        <div class="headline text-xs-center">Completely Open Sourced</div>
+                      </v-card-title>
+                      <v-card-text>
+                        Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                        Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
+                      </v-card-text>
+                    </v-card>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-flex>
+          </v-layout>
+        </section>
+
+        <section>
+          <v-parallax src="images/1.jpg" height="600">
+            <v-layout
+              column
+              align-center
+              justify-center
+              class="white--text"
+            >
+              <img src="images/logo.png" alt="Vuetify.js" height="200">
+              <h1 class="white--text mb-2 display-1 text-xs-center">Parallax Template</h1>
+              <div class="subheading mb-3 text-xs-center">Powered by Vuetify</div>
+              <v-btn
+                class="blue lighten-2 mt-5"
+                dark
+                large
+                href="/pre-made-themes"
+              >
+                Get Started
+              </v-btn>
+            </v-layout>
+          </v-parallax>
+        </section>
+
+        <section>
+          <v-layout
+            column
+            wrap
+            class="my-5"
+            align-center
+          >
+            <!-- <v-flex xs12 sm4 class="my-3">
+              <div class="text-xs-center">
+                <h2 class="headline">The best way to start developing</h2>
+                <span class="subheading">
+                  Cras facilisis mi vitae nunc 
+                </span>
+              </div>
+            </v-flex> -->
+            <v-flex xs12>
+              <v-container grid-list-xl>
+                <v-layout row wrap align-center>
+                  <v-flex xs12 md4>
+                    <v-card class="elevation-0 transparent">
+                      <v-card-text class="text-xs-center">
+                        <v-icon x-large class="blue--text text--lighten-2">color_lens</v-icon>
+                      </v-card-text>
+                      <v-card-title primary-title class="layout justify-center">
+                        <div class="headline text-xs-center">Material Design</div>
+                      </v-card-title>
+                      <v-card-text>
+                        Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                        Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
+                      </v-card-text>
+                    </v-card>
+                  </v-flex>
+                  <v-flex xs12 md4>
+                    <v-card class="elevation-0 transparent">
+                      <v-card-text class="text-xs-center">
+                        <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
+                      </v-card-text>
+                      <v-card-title primary-title class="layout justify-center">
+                        <div class="headline">Fast development</div>
+                      </v-card-title>
+                      <v-card-text>
+                        Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                        Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
+                      </v-card-text>
+                    </v-card>
+                  </v-flex>
+                  <v-flex xs12 md4>
+                    <v-card class="elevation-0 transparent">
+                      <v-card-text class="text-xs-center">
+                        <v-icon x-large class="blue--text text--lighten-2">build</v-icon>
+                      </v-card-text>
+                      <v-card-title primary-title class="layout justify-center">
+                        <div class="headline text-xs-center">Completely Open Sourced</div>
+                      </v-card-title>
+                      <v-card-text>
+                        Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                        Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
+                      </v-card-text>
+                    </v-card>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-flex>
+          </v-layout>
+        </section>
+
+        <v-footer class="blue darken-2">
+          <v-layout row wrap align-center>
+            <v-flex xs12>
+              <div class="white--text ml-3">
+                Made with
+                <v-icon class="red--text">favorite</v-icon>
+                by <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
+                and <a class="white--text" href="https://github.com/vwxyzjn">Costa Huang</a>
+              </div>
+            </v-flex>
+          </v-layout>
+        </v-footer>
+      </v-content>
+    </v-app>
   </div>
 </template>
 
@@ -9,6 +211,11 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'index'
+  name: 'index',
+  data () {
+    return {
+      title: 'YUSHARE.CN'
+    }
+  }
 }
 </script>
