02167b7b1d59b5ae878bbc281426d23a0f383776
yushare <582718643@qq.com>
2018-12-19 15:09:43 +0800

    update config

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,28 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+  export default {
+    name: 'about',
+    data () {
+      return {
+
+      }
+    },
+    mounted() {
+      let func = () => {
+        console.log(+ new Date)
+      }
+      func()
+    },
+  }
+</script>
+
+<style scoped lang="scss">
+  .about {
+    h1 {
+      background: linear-gradient(-45deg, #FE8727 0em, #FE8727 0.9em, #FE4500 1em, #FE4500 2.1em, #FE8727 2.1em, #FE8727 2.9em, #FE4500 3em);
+    }
+  }
+</style>
